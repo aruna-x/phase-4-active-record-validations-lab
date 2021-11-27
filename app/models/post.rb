88eff1a731624c157a4b1a3eba886fe4a29c28ca
Aruna <aruna.evan15@gmail.com>
2021-11-27 15:56:39 -0800

    commit tree 43baf9b1440394ddb4071a5f3fcf005bcbb7a09a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validate :title, :is_clickbait  
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+    def is_clickbait
+        if title
+            unless (title.include?("Won't Believe") || title.include?("Secret") || title.include?("Top") || title.include?("Guess"))
+                errors.add(:title, "Title isn't clickbait-y enough!")
+            end
+        end
+    end
 end
